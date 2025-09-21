3689a222f38f1798ee6c77a5212efbc6d96e403c
Christian Ludwig <christian.ludwig@auto-eckhardt.com>
2025-09-21 03:40:56 +0200

    Replace Teile with Terminplaner in nav + redirect /teile -> /terminplaner

---
+++ nextconfig.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/teile',
+        destination: '/terminplaner',
+        permanent: true, // 308/301
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
